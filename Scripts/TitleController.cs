4d8dd2ee2b8087dea577a466e93f162786361dc7
Dyna-Rise <dynarise.headquarters@gmail.com>
2024-12-24 10:07:02 +0900

    12/23 10:06 TitleHighScoreText Added

--- Scripts/TitleController.cs
+++ Scripts/TitleController.cs
@@ -2,13 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class TitleController : MonoBehaviour
 {
+    public TextMeshProUGUI highScoreText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //PlayerPrefsから取得したスコアをHighScoreTextオブジェクトに表示
+        highScoreText.text = "High Score:" + PlayerPrefs.GetInt("HighScore") + "m";
     }
 
     // Update is called once per frame
